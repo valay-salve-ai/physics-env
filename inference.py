b43311445192c0a66faeec875417f0113309afd3
valay-salve-ai <valaysalve198@gmail.com>
2026-04-08 22:23:28 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -12,7 +12,7 @@ from openai import OpenAI
 # ==============================================================================
 # from my_env_v4 import MyEnvV4Action, MyEnvV4Env  <-- COMMENTED OUT (Placeholder only)
 IMAGE_NAME = os.getenv("IMAGE_NAME") # If you are using docker image 
-API_KEY = os.getenv("HF_TOKEN") or os.getenv("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")
+API_KEY = os.getenv("HF_TOKEN") or os.getenv("")
 
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
 MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
